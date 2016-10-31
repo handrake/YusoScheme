4fc50af8f98d20690ec43248d827a88cce530a86
Byoung Gi Lee <handrake@gmail.com>
2016-10-31 17:33:17 +0900

    moved eval() to outside to avoid class declaration conflicts

--- YusoScheme/main.cpp
+++ YusoScheme/main.cpp
@@ -49,6 +49,33 @@ bool IsPrimitiveType(ExpressionTypes type) {
 	return false;
 }
 
+class Expression {
+private:
+	ExpressionTypes type_;
+	Environment *env_;
+	ProcType proc_;
+public:
+	vector<Expression> list;
+	string val;
+
+	Expression(ExpressionTypes type = kSymbol) : type_(type), env_(nullptr) {}
+	Expression(ExpressionTypes type, const string &val) : type_(type), val(val), env_(nullptr) {}
+	Expression(ProcType proc) : type_(kProc), proc_(proc) {}
+
+	ExpressionTypes get_type() const {
+		return type_;
+	}
+
+	void set_env(Environment *env) {
+		env_ = env;
+	}
+
+	friend ostream& operator<<(ostream &os, const Expression &exp) {
+		os << exp.val;
+		return os;
+	}
+};
+
 class Environment {
 private:
 	Environment *other_;
@@ -77,6 +104,22 @@ public:
 	}
 };
 
+Expression eval(Expression *exp, Environment *env = global_env) {
+	if (exp->get_type() == kSymbol)
+		return *env->find(exp->val)->at(exp->val);
+	else if (exp->get_type() != kList) {
+		return *exp;
+	}
+	else if (exp->list.empty()) {
+		return *env->find("nil")->at("nil");
+	}
+	else if (exp->list[0].val == "define") {
+		//return env[this->list[1].val] = this->list[2].eval();
+	}
+	return *exp;
+}
+
+
 list<string> tokenize(string &s) {
 	list<string> tokens;
 
@@ -98,47 +141,6 @@ list<string> tokenize(string &s) {
 
 	return tokens;
 }
-
-class Expression {
-private:
-	ExpressionTypes type_;
-	Environment *env_;
-	ProcType proc_;
-public:
-	vector<Expression> list;
-	string val;
-
-	Expression(ExpressionTypes type = kSymbol) : type_(type), env_(nullptr) {}
-	Expression(ExpressionTypes type, const string &val) : type_(type), val(val), env_(nullptr) {}
-	Expression(ProcType proc) : type_(kProc), proc_(proc) {}
-
-	ExpressionTypes get_type() const {
-		return type_;
-	}
-
-	void set_env(Environment *env) {
-		env_ = env;
-	}
-
-	Expression eval(Environment *env = global_env) {
-		if (this->get_type() == kSymbol)
-			return *env->find(this->val)->at(this->val);
-		else if (this->get_type() != kList) {
-			return *this;
-		}
-		else if (this->list.empty()) {
-			return *env->find("nil")->at("nil");
-		}
-		else if (this->list[0].val == "define") {
-			//return env[this->list[1].val] = this->list[2].eval();
-		}
-		return *this;
-	}
-	friend ostream& operator<<(ostream &os, const Expression &exp) {
-		os << exp.val;
-		return os;
-	}
-};
 
 Expression atom(const string &token) {
 	if (regex_match(token, regex("#[tf]"))) {
@@ -199,7 +201,7 @@ void repl(Environment *env) {
 		getline(cin, line);
 		if (regex_match(line, regex("[ ]*")))
 			continue;
-		cout << parse(line).eval() << endl;
+		cout << eval(&parse(line)) << endl;
 	}
 }
 
